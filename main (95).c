c30435b777eadda447a6a76998a2e756dcf00cde
Tinaiana <161570552+Tinaiana@users.noreply.github.com>
2025-05-03 22:10:55 +0330

    Update main (95).c

--- main (95).c
+++ main (95).c
@@ -1,72 +1,88 @@
-#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
 
-#define SIZE 9  // size
+#define SIZE 9
 
-int sudoku[SIZE][SIZE];  
+int board[9][9] = {
+    {6,2,4,5,3,9,1,8,7},
+    {5,1,9,7,2,8,6,3,4},
+    {8,3,7,6,1,4,2,9,5},
+    {1,4,3,8,6,5,7,2,9},
+    {9,5,8,2,4,7,3,6,1},
+    {7,6,2,3,9,1,4,5,8},
+    {3,7,1,9,5,6,8,4,2},
+    {4,9,6,1,8,2,5,7,3},
+    {2,8,5,4,7,3,9,1,6}
+};
 
-// checking row
-void* check_row(void* arg) {
-    int row = *((int*) arg);
-    int check[SIZE] = {0};
+int valid[11]; // 0: rows, 1: cols, 2-10: boxes
 
+void* check_rows(void* arg) {
     for (int i = 0; i < SIZE; i++) {
-        int num = sudoku[row][i];
-        if (check[num - 1] == 1) {
-            pthread_exit((void*) 0);  // invalid
+        int seen[10] = {0};
+        for (int j = 0; j < SIZE; j++) {
+            int val = board[i][j];
+            if (val < 1 || val > 9 || seen[val]) pthread_exit(NULL);
+            seen[val] = 1;
         }
-        check[num - 1] = 1;
     }
-
-    pthread_exit((void*) 1);  // valid
+    valid[0] = 1;
+    pthread_exit(NULL);
 }
 
-// checking colum
-void* check_column(void* arg) {
-    int col = *((int*) arg);
-    int check[SIZE] = {0};
-
-    for (int i = 0; i < SIZE; i++) {
-        int num = sudoku[i][col];
-        if (check[num - 1] == 1) {
-            pthread_exit((void*) 0);  // invalid
+void* check_cols(void* arg) {
+    for (int j = 0; j < SIZE; j++) {
+        int seen[10] = {0};
+        for (int i = 0; i < SIZE; i++) {
+            int val = board[i][j];
+            if (val < 1 || val > 9 || seen[val]) pthread_exit(NULL);
+            seen[val] = 1;
         }
-        check[num - 1] = 1;
     }
-
-    pthread_exit((void*) 1);  // valid
+    valid[1] = 1;
+    pthread_exit(NULL);
 }
 
+void* check_box(void* arg) {
+    int idx = *(int*)arg;
+    int row = (idx / 3) * 3;
+    int col = (idx % 3) * 3;
+    int seen[10] = {0};
+    for (int i = row; i < row + 3; i++) {
+        for (int j = col; j < col + 3; j++) {
+            int val = board[i][j];
+            if (val < 1 || val > 9 || seen[val]) pthread_exit(NULL);
+            seen[val] = 1;
+        }
+    }
+    valid[2 + idx] = 1;
+    pthread_exit(NULL);
+}
 
 int main() {
-    
-    
-    pthread_t threads[SIZE * 2];  
-    int row_indices[SIZE], col_indices[SIZE];
-    
-    
-    for (int i = 0; i < SIZE; i++) {
-        row_indices[i] = i;
-        pthread_create(&threads[i], NULL, check_row, &row_indices[i]);
+    pthread_t threads[11];
+    int box_ids[9];
+
+    pthread_create(&threads[0], NULL, check_rows, NULL);
+    pthread_create(&threads[1], NULL, check_cols, NULL);
+
+    for (int i = 0; i < 9; i++) {
+        box_ids[i] = i;
+        pthread_create(&threads[2 + i], NULL, check_box, &box_ids[i]);
     }
 
-    
-    for (int i = 0; i < SIZE; i++) {
-        col_indices[i] = i;
-        pthread_create(&threads[SIZE + i], NULL, check_column, &col_indices[i]);
-    }
+    for (int i = 0; i < 11; i++)
+        pthread_join(threads[i], NULL);
 
-    // checking for results
-    for (int i = 0; i < SIZE * 2; i++) {
-        void* result;
-        pthread_join(threads[i], &result);
-        if (result == (void*) 0) {
-            printf("it is not okay\n");
-            return 0;
+    int all_ok = 1;
+    for (int i = 0; i < 11; i++) {
+        if (!valid[i]) {
+            all_ok = 0;
+            break;
         }
     }
 
-    printf("it is okay\n");
+    printf("%s\n", all_ok ? " OK" : " Not OK");
     return 0;
 }
